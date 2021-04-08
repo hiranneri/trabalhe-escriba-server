b8eafb20a26cccc9a6a291a9552e714eb0649186
Hiran <hiran.souza93@gmail.com>
2021-04-08 20:12:34 -0300

    Configurações para o deploy com heroku

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+console.log('Ambiente: '+ process.env.DATABASE_URL)
+module.exports = {
+  "url": process.env.DATABASE_URL,
+  "type": "postgres",
+  "entities": [
+    process.env.ENTITIES_URL
+ ],
+ "migrations": [
+  process.env.MIGRATIONS_URL
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations"
+  ],
+  "entitiesDir": "src/models"
+  }
+}
